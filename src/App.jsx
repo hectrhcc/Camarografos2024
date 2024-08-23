696dd8129ff0a346fec911db0b8a8835b6c901f8
Hector <hcontrerascorvacho@gmail.com>
2024-08-23 18:01:02 -0400

    orden font bold y semibold

--- src/App.jsx
+++ src/App.jsx
@@ -104,8 +104,8 @@ function App() {
           <li>
           1) <span className='underline font-bold'>Presentación y motivo de la llamada</span>: Preséntese y confirme fecha, horario y dirección de la grabación, datos previamente entregados por el CTG.</li>
         <ol type="a">
-          <li className='ml-4 mt-2'>a) <b>Camarógrafo/a</b>: Buenos días, ¿hablo con el/la <b>docente</b> (nombre de el/la docente)?</li>
-          <li className='ml-4 mt-2'>b) Le llamo desde <b>“DocenteMás”, mi nombre es</b> (su nombre) <b>y soy el camarógrafo/a asignado para grabarlo/a el día</b>
+          <li className='ml-4 mt-2'>a) <span className='font-semibold'>Camarógrafo/a</span>: Buenos días, ¿hablo con el/la <span className='font-semibold'>docente</span> (nombre de el/la docente)?</li>
+          <li className='ml-4 mt-2'>b) Le llamo desde <span className='font-semibold'>“DocenteMás”, mi nombre es</span> (su nombre) <span className='font-semibold'>y soy el camarógrafo/a asignado para grabarlo/a el día</span>
                   (la fecha de grabación) a las (hora de la grabación), en la dirección (dirección del lugar de grabación).</li>
           <li className='ml-4 mt-2'>c) ¿Usted efectúa la especialidad de (indique la especialidad informada por su CTG)?. En caso de especificar una distinta,
                   debe informar a la brevedad a su CTG.</li>
@@ -113,20 +113,20 @@ function App() {
         <li className='mt-4 '>2) <span className='underline font-bold'>Propósito de la llamada</span>: Solicítele que confirme en qué sala o espacio del colegio realizará la clase a ser grabada: Existen
 dos posibles respuestas:</li>
         <ol type="a">
-          <li className='ml-4 mt-2'>a) <span className='underline'>En Sala de clases</span>: Si indica que la clase <span className='underline'><b>será efectuada en una Sala de clases</b> tradicional</span>; entonces debe saltar a la 
+          <li className='ml-4 mt-2'>a) <span className='underline'>En Sala de clases</span>: Si indica que la clase <span className='underline'><span className='font-semibold'>será efectuada en una Sala de clases</span> tradicional</span>; entonces debe saltar a la 
             <b> pregunta 3</b>. Entonces, Usted deberá efectuar la grabación como se establece en el punto 4.1 de este instructivo, enten-
             diendo que el nivel de ruido no será un elemento que atente o perjudique la grabación de el/la docente.</li>
-          <li className='ml-4 mt-2'>b) <span className='underline'>En un espacio o dependencia distinta a una Sala de Clases</span>: el/la docente Indica que la <span className='underline font-bold'>realizará en un espacio
+          <li className='ml-4 mt-2'>b) <span className='underline'>En un espacio o dependencia distinta a una Sala de Clases</span>: el/la docente Indica que la <span className='underline font-semibold'>realizará en un espacio
             diferente a una Sala de Clases, por ejemplo: en un taller, al aire libre u otro espacio</span>. Infórmele que, para un mejor
             registro del audio, utilizará un Micrófono inalámbrico. Continúe en la pregunta 3.</li>
         </ol>
         <li className='mt-4'>3) <span className='underline font-bold'>Pregúntele sobre la o las actividades</span> que tiene planificadas para su clase y si usted debe tener algún cuidado especial.
 Indague sobre los siguientes aspectos:</li>
-<li><b>Camarógrafo/a</b>: Necesito preguntarle sobre algunos aspectos de la clase que desarrollará:</li>
+<li><span className='font-semibold'>Camarógrafo/a</span>: Necesito preguntarle sobre algunos aspectos de la clase que desarrollará:</li>
 <ol type="a">
 <li className='ml-4 mt-2'>a) La o las actividades que se efectuarán en la clase: Indíqueme si debo tener algún cuidado en mi rol de camarógrafo en los desplazamientos que eventualmente debo efectuar al grabar. Por ejemplo, con herramientas, artefactos o utensilios
 que los estudiantes puedan utilizar.</li>
-<li className='ml-4 mt-2'>b) En la actividad a realizar durante la clase, <b>Si tiene planificado</b> el uso de herramientas, artefactos o utensilios que
+<li className='ml-4 mt-2'>b) En la actividad a realizar durante la clase, <span className='font-semibold'>Si tiene planificado</span> el uso de herramientas, artefactos o utensilios que
 puedan generar riesgo al realizar desplazamientos durante la grabación, como por ejemplo si en:</li>
 <ul>
 <li className='ml-10'>- Una clase de cocina, utilizarán batidoras,</li>
@@ -136,10 +136,10 @@ puedan generar riesgo al realizar desplazamientos durante la grabación, como po
 <li className='ml-10'>- U otras actividades.</li>
 </ul>
 </ol>
-<li className='mt-4'>4) <span className='underline font-bold'>Cierre del contacto telefónico</span>: Según lo conversado, <b>le confirmo la siguiente información</b>:</li>
+<li className='mt-4'>4) <span className='underline font-bold'>Cierre del contacto telefónico</span>: Según lo conversado, <span className='font-semibold'>le confirmo la siguiente información</span>:</li>
 <ol type="a">
 <li className='ml-4 mt-2'>
-a) <b>Su nombre es</b> (nombre del o la docente), la grabación se realizará el (fecha de grabación), a las (hora de la grabación),
+a) <span className='font-semibold'>Su nombre es</span> (nombre del o la docente), la grabación se realizará el (fecha de grabación), a las (hora de la grabación),
 en (dirección de la grabación).</li>
 <li className='ml-4 mt-2'>
 b) Confirma que su clase se realizará en una Sala de Clases: SI / NO.</li>
@@ -150,9 +150,9 @@ problemas en la calidad de audio a registrar.</li>
 <li  className='ml-10'><span className='underline'>Caso NO</span>: En caso de efectuar la clase en taller u aire libre u otra dependencia distinta a una sala de clases, se debe
 utilizar Micrófono inalámbrico para grabar correctamente el audio.</li>
 </ul>
-<li className='ml-4 mt-2'>c) La grabación de su clase requiere que (<b>Debe especificar las recomendaciones indicadas por el/la docente</b>).</li>
-<li className='ml-4 mt-2'>d) Recuérdele a el/la docente: <b>“Favor, recuerde anticiparles a los y las estudiantes sobre mi visita”</b>.</li>
-<li className='ml-4 mt-2 mb-8'>e) <b>Despedida</b>: Gracias por su atención y por los datos proporcionados, que tenga un buen día.</li>
+<li className='ml-4 mt-2'>c) La grabación de su clase requiere que (<span className='font-semibold'>Debe especificar las recomendaciones indicadas por el/la docente</span>).</li>
+<li className='ml-4 mt-2'>d) Recuérdele a el/la docente: <span className='font-semibold'>“Favor, recuerde anticiparles a los y las estudiantes sobre mi visita”</span>.</li>
+<li className='ml-4 mt-2 mb-8'>e) <span className='font-semibold'>Despedida</span>: Gracias por su atención y por los datos proporcionados, que tenga un buen día.</li>
 </ol>
         </ul>
         </div>
@@ -164,32 +164,32 @@ utilizar Micrófono inalámbrico para grabar correctamente el audio.</li>
         <div className='mx-auto ml-8 xs:ml-6 mt-4 mb-3'>
           <h2 className='font-bold text-justify mb-2'>Instrucciones antes de ingresar :</h2>
 
-            <div className='text-justify '>- Llegar <b>15 a 20 minuto</b> antes</div>
-            <div className='text-justify '>- <b>Apagar celular</b> antes de entrar al Establecimiento </div>
-            <div className='text-justify'>- Si se graba con smarthpone activar <b>modo avión</b></div>
-            <div className='text-justify'>- Confirmar que lleva <b>todos los materiales y el equipamiento</b> de ese día</div>
+            <div className='text-justify '>- Llegar <span className='font-semibold'>15 a 20 minuto</span> antes</div>
+            <div className='text-justify '>- <span className='font-semibold'>Apagar celular</span> antes de entrar al Establecimiento </div>
+            <div className='text-justify'>- Si se graba con smarthpone activar <span className='font-semibold'>modo avión</span></div>
+            <div className='text-justify'>- Confirmar que lleva <span className='font-semibold'>todos los materiales y el equipamiento</span> de ese día</div>
             <div className='text-justify'>- Recuerde ingresar utilizando su casaca y credencial de camarógrafo/a acreditado/a</div>
           <h2 className='font-bold text-justify mt-4 mb-2'>Protocolo:</h2>
-            <div className='text-justify '>- Llegar <b>15 a 20 minutos</b> antes</div>
-            <div className='text-justify '>- Preséntese con su <b>nombre y apellido</b></div>
-            <div className='text-justify '>- Indique que usted es “<b>camarógrafo/a acreditado/a de DocenteMás</b>” y motivo de su visita</div>
+            <div className='text-justify '>- Llegar <span className='font-semibold'>15 a 20 minutos</span> antes</div>
+            <div className='text-justify '>- Preséntese con su <span className='font-semibold'>nombre y apellido</span></div>
+            <div className='text-justify '>- Indique que usted es “<span className='font-semibold'>camarógrafo/a acreditado/a de DocenteMás</span>” y motivo de su visita</div>
             <div className='text-justify '>- Señale el nombre de el o los docentes o educadores/as a grabar.</div>
             <div className='text-justify '>- Solicite hablar con la persona encargada del proceso en el establecimiento</div>
           
             <h2 className='font-bold text-justify mt-4 mb-2'>Instrucciones ingresar aula:</h2>
             <div className='text-justify '>- Saludar al docente de manera cordial </div>
-            <div className='text-justify '>- Presentarse como <b>“Camarógrafo/a acreditado/a de DocenteMás”</b>, señalando su nombre y apellido</div>
+            <div className='text-justify '>- Presentarse como <span className='font-semibold'>“Camarógrafo/a acreditado/a de DocenteMás”</span>, señalando su nombre y apellido</div>
             <div className='text-justify '>- Solicite a el/la docente que informe a los estudiantes de su presencia de manera discreta y breve</div>
             <div className='text-justify '>- Determinar ubicación del equipamiento de grabación en el aula</div>
-            <div className='text-justify '>- En caso de ser necesario, <b>cierre las ventanas</b> para evitar el ruido exterior.</div>
+            <div className='text-justify '>- En caso de ser necesario, <span className='font-semibold'>cierre las ventanas</span> para evitar el ruido exterior.</div>
             <div className='text-justify '>- Ubique la cámara desde el costado donde las ventanas queden a su espalda.</div>
-            <div className='text-justify '>- <b>Cierre las cortinas</b> que están en el lado opuesto a donde
+            <div className='text-justify '>- <span className='font-semibold'>Cierre las cortinas</span> que están en el lado opuesto a donde
             se ubicaría usted para evitar el contraluz</div>
             <div className='text-justify '>- Si no hay cortinas, sitúese a espaldas del sector que recibe mayor cantidad de luz</div>
             <div className='text-justify '>- Realice pruebas para verificar si cuenta con una imagen nítida y sin contraluz</div>
-            <div className='text-justify '>- Si el aula tiene ventanales por todos lados, <b>maneje la altura de la cámara y su inclinación</b></div>
+            <div className='text-justify '>- Si el aula tiene ventanales por todos lados, <span className='font-semibold'>maneje la altura de la cámara y su inclinación</span></div>
             <div className='text-justify '>- Ubiquese teniendo presente que debe lograr enfocar al docente de perfil o de frente</div>
-            <div className='text-justify '>- Si usa proyector en la clase, debe realizar <b>pruebas de luminosidad</b> para no quedar a oscuras/contraluz</div>
+            <div className='text-justify '>- Si usa proyector en la clase, debe realizar <span className='font-semibold'>pruebas de luminosidad</span> para no quedar a oscuras/contraluz</div>
             <div className='text-justify '>- <span className='font-semibold'>Montaje del equipamiento</span></div>
             <div className='text-justify ml-4'>- Instale su trípode y su cámara de video</div>
             <div className='text-justify ml-4'>- Compruebe que el nivel de carga de su batería</div>
@@ -200,23 +200,23 @@ utilizar Micrófono inalámbrico para grabar correctamente el audio.</li>
             <div className='text-justify ml-4'>- Facilité al docente lápiz (plumón)</div>
           <h2 className='font-bold text-justify mt-4 mb-2'>Iniciar Grabación:</h2>  
           <div className='text-justify '>- Grabar por 5 segundos el RUT de el/la docente</div>
-          <div className='text-justify '>- Retire el Sobre Módulo 2 y diga en voz alta “<b>GRABANDO</b>”</div>
+          <div className='text-justify '>- Retire el Sobre Módulo 2 y diga en voz alta “<span className='font-semibold'>GRABANDO</span>”</div>
           <div className='text-justify '>- Realizar 1 Plano General con paneo, describa toda la sala con sus participantes</div>
           <div className='text-justify '>- Realizar durante toda la clase secuencia de paneos en gran angular, hasta que finalice la clase</div>
-          <div className='text-justify ml-4 mt-1'>- Panee en gran angular lentamente y <b>sin detenerse en ningún grupo/estudiante</b> por más de 10 seg</div>
-          <div className='text-justify ml-4 '>- <b>Siempre debe grabar en gran angular</b>. A menos que cuando en la sala de clases se <b>produzcan interacciones </b>
+          <div className='text-justify ml-4 mt-1'>- Panee en gran angular lentamente y <span className='font-semibold'>sin detenerse en ningún grupo/estudiante</span> por más de 10 seg</div>
+          <div className='text-justify ml-4 '>- <span className='font-semibold'>Siempre debe grabar en gran angular</span>. A menos que cuando en la sala de clases se <span className='font-semibold'>produzcan interacciones </span>
  entre docente y estudiantes</div>
-<div className='text-justify ml-4 '>- Si <b>durante el paneo</b> hay interacciones, usted puede <b>utilizar el zoom y cerrarse en plano conjunto</b></div>
-<div className='text-justify ml-4 '>- Estas tomas deben efectuarse <b>NO MÁS</b> de 10 veces y por un tiempo máximo de 10 segundos por toma</div>
+<div className='text-justify ml-4 '>- Si <span className='font-semibold'>durante el paneo</span> hay interacciones, usted puede <span className='font-semibold'>utilizar el zoom y cerrarse en plano conjunto</span></div>
+<div className='text-justify ml-4 '>- Estas tomas deben efectuarse <span className='font-semibold'>NO MÁS</span> de 10 veces y por un tiempo máximo de 10 segundos por toma</div>
 <div className='text-justify ml-4 '>- Se encuentra con información en el pizarrón o en la proyección, realice zoom (Basta 1 vez en la grabación)</div>
 <div className='text-justify ml-4 '>- Vuelva a gran angular y siga la secuencia de paneos</div>
-<div className='text-justify ml-4 '>- <b>NO</b> haga zoom en guías, cuadernos o libros de los y las estudiantes</div>
+<div className='text-justify ml-4 '>- <span className='font-semibold'>NO</span> haga zoom en guías, cuadernos o libros de los y las estudiantes</div>
 <div className='text-justify ml-4 '>- Monitoree constantemente el tiempo de grabación en el visor de su cámara</div>
 <div className='text-justify ml-4 '>- Siga la secuencia de paneos hasta el fin de la clase</div>
 
-          <div className='text-justify '>- Indique al docente cuando se completen 35 minutos (<b>levantando la mano</b>)</div>
+          <div className='text-justify '>- Indique al docente cuando se completen 35 minutos (<span className='font-semibold'>levantando la mano</span>)</div>
           <div className='text-justify '>- Finalizada la clase, grabe 10 segundos adicionales y luego detenga la grabación</div>
-          <div className='text-justify '>- <b>DEBE parar la grabación al minuto 45 de la clase, como máximo</b></div>
+          <div className='text-justify '>- <span className='font-semibold'>DEBE parar la grabación al minuto 45 de la clase, como máximo</span></div>
           <h2 className='font-bold text-justify mt-4 mb-2'>Casos especiales Sin Paneo</h2>
           <ol type='a'>
           <li>a) Caso sala de clase con pocos estudiantes:</li>
